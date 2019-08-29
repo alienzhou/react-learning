25073e1a3c56e261fb9b0543dbeee182e4a884da
zhouhongxuan <zhouhongxuan@baidu.com>
2019-08-29 17:47:21 +0800

    build(alienact):

--- alienact/packages/alienact-core/rollup.config.js
+++ alienact/packages/alienact-core/rollup.config.js
@@ -1,8 +1,43 @@
 // cSpell: ignore alienact, sourcemap
 const path = require('path');
-const createConfig = require('../tools/rollup.config.creator');
+const babel = require('rollup-plugin-babel');
+const typescript = require('rollup-plugin-typescript2'); // notice ts version (https://github.com/ezolenko/rollup-plugin-typescript2/issues/88)
+const {uglify} = require('rollup-plugin-uglify');
 
-module.exports = createConfig({
-    subModuleName: 'core',
-    sourceBase: __dirname
-});
+const MODULE_NAME = 'alienact-core';
+const ENTRY = path.resolve(__dirname, 'src/index.ts');
+const DEST = path.resolve(__dirname, '..', '..', 'build/', MODULE_NAME);
+
+module.exports = [{
+    input: ENTRY,
+    plugins: [
+        typescript({
+            tsconfig: 'tsconfig.json'
+        }),
+        babel({
+            runtimeHelpers: true
+        }),
+        uglify()
+    ],
+    output: [{
+        file: path.resolve(DEST, `${MODULE_NAME}.js`),
+        format: 'umd',
+        name: MODULE_NAME,
+        sourcemap: true,
+        globals: 'window'
+    }]
+}, {
+    input: ENTRY,
+    plugins: [
+        typescript({
+            tsconfig: 'tsconfig.json'
+        })
+    ],
+    output: [{
+        file: path.resolve(DEST, `${MODULE_NAME}.esm.js`),
+        format: 'esm',
+        name: MODULE_NAME,
+        sourcemap: true,
+        globals: 'window'
+    }]
+}]
